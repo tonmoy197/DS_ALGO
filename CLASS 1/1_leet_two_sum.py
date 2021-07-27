29a4bc2c7fc43c477d7bfbe49146120466f29cb2
tonmoy197 <tonmoydas197@gmail.com>
2021-07-27 17:19:23 +0600

    Update readme file

--- CLASS 1/1_leet_two_sum.py
+++ CLASS 1/1_leet_two_sum.py
@@ -3,17 +3,56 @@ class Solution:
         pass
 
 
-    def doublefor(self,nums, target):
-        for i in range(len(nums)/2):
-            for j in range(len(nums)):
-                if nums[i]+nums[j] == target :
-                    print([i,j])
+    # def doublefor1(self,nums, target):
+    #     for i in range(int(len(nums)/2)):
+    #         for j in range(len(nums)):
+    #             if nums[i]+nums[j] == target :
+    #                 print([i,j])
 
-nums=[2,7,11,15]
-target=9
+    # def doublefor(self, nums, target):
+    #     k = 0
+    #     for element in nums:
+    #         j = target - element
+    #         k += 1
+    #         tmp_nums = nums[k:] #creates a list of 1:last
+    #         if j in tmp_nums:
+    #             return [k - 1, tmp_nums.index(j) + k]
+    def doublefor(self, nums, target):
+        import copy
+        sort_nums = copy.deepcopy(nums)
+        sort_nums.sort()
+        i = 0
+        j = len(sort_nums) - 1
+        while i < j:
+            if sort_nums [i] + sort_nums [j] == target:
+                break
+            elif sort_nums [i] + sort_nums [j] > target:
+                j -= 1
+            else:
+                i += 1
+        if i >= j:
+            return []
+        index1 = None
+        index2 = None
+        for k in range(len(nums)):
+            if index1 is None and nums [k] == sort_nums [i]:
+                index1 = k
+            elif index2 is None and nums [k] == sort_nums [j]:
+                index2 = k
+        answer = [index1 + 1, index2 + 1]
+        answer.sort()
+        return answer
+
+nums1=[2,7,11,4]
+nums2=[3,2,4]
+nums3=[3,3]
+target=6
 
 
-Solution.doublefor(nums,target)
+
+print(Solution().doublefor(nums1,target))
+print(Solution().doublefor(nums2,target))
+print(Solution().doublefor(nums3,target))
 
 
 
---
+++ README.md
@@ -0,0 +1,17 @@
+# what is data structure?
+Arrangement of data so that they can be used efficiently in memory.
+
+# What is algorithm?
+Sequence of steps on data using efficient data structure to solve a given problem.
+
+# What is time complexity?
+How time taken to execute an algorithm grows with the size of the input.
+
+# What is Big Oh notation (O)?
+This is related to time and space increases with respect to input.
+Used to describe osymptotic upper bound . 
+  Mathmathecally, A function f(n) siad to be Big Oh if and only if there exist positive constants C and n(not) such that 
+  0<=f(x)<=cg(n) for all n>n(not)
+
+# Example of Big Oh notation?
+O(n) ,O(logn) ,O(n^2)
