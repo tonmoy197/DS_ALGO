7d387a0ed783ac26a73b481f23205f9382c9e969
tonmoy197 <tonmoydas197@gmail.com>
2021-07-27 17:30:04 +0600

    Update reverse_a_list.py

--- CLASS 1/reverse_a_list.py
+++ CLASS 1/reverse_a_list.py
@@ -1,7 +1,10 @@
-li =[1,2,3,4,5,6]
-for i in range(len(li)):
-    a=li[len(li)-1-i]
-    li[i]=a
-
-# i am commit no 2
-print(li)
+a = [1,2,3,4,5,6]
+lo=0
+hi=len(a)-1
+while lo<hi:
+    tmp = a[lo]
+    a[lo]=a[hi]
+    a[hi]=tmp
+    lo+=1
+    hi-=1
+print(a) 
